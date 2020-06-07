e3d0cf04ae488bc0a72897fc7daf7a9309b35407
Andrew R Leonardi <ndrewleonardi2@gmail.com>
2020-06-07 01:10:15 -0700

    [test] Added tests to card component

--- src/ui/Card.js
+++ src/ui/Card.js
@@ -1,27 +1,29 @@
 import React from "react";
-import Button from "./Button";
+import SaveButton from "./SaveButton";
 
-export default ({ comments, likes, webformatURL, tags }) => (
-  <div className="card">
-    <div className="card-image">
-      <img src={webformatURL}></img>
-      <Button title={`Save`} type="saved" />
-    </div>
+export default ({ id, comments, likes, webformatURL, tags }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={webformatURL}></img>
+        <SaveButton id={id} />
+      </div>
 
-    <div className="card-tags">
-      <ul>
-        {tags.split(",").map((tag, i) => (
-          <li key={i}>{tag}</li>
-        ))}
-      </ul>
+      <div className="card-tags">
+        <ul>
+          {tags.split(",").map((tag, i) => (
+            <li key={i}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="card-stars">
+        <span data-testid="comments">{comments}</span>
+        <i className="icon-star"></i>
+      </div>
+      <div className="card-comments">
+        <span data-testid="likes">{likes}</span>
+        <i className="icon-thumbsup"></i>
+      </div>
     </div>
-    <div className="card-stars">
-      <span>{comments}</span>
-      <i className="icon-star"></i>
-    </div>
-    <div className="card-comments">
-      <span>{likes}</span>
-      <i className="icon-thumbsup"></i>
-    </div>
-  </div>
-);
+  );
+};
